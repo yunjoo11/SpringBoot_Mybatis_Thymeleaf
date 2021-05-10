3db28c0cd0afc1914d35132e18f1c24f65b75422
yunjoo11 <qodbswn11@naver.com>
2021-05-10 14:36:20 +0900

    init

---
+++ SpringBoot_Mybatis_Tymeleaf/src/main/java/com/joo/s1/SpringBootMybatisTymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.joo.s1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMybatisTymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMybatisTymeleafApplication.class, args);
+	}
+
+}
---
+++ SpringBoot_Mybatis_Tymeleaf/src/test/java/com/joo/s1/SpringBootMybatisTymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joo.s1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMybatisTymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
