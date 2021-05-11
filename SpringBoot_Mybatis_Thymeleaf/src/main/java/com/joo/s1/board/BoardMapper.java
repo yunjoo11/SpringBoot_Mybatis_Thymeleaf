6945257cda37349eaed4cafd7d6388f8b1d13691
yunjoo11 <qodbswn11@naver.com>
2021-05-11 12:35:04 +0900

    board init

---
+++ SpringBoot_Mybatis_Thymeleaf/src/main/java/com/joo/s1/board/BoardMapper.java
@@ -0,0 +1,26 @@
+package com.joo.s1.board;
+
+import java.util.List;
+
+public interface BoardMapper {
+
+	//List
+	public List<BoardVO> getList()throws Exception;
+	
+	//count
+	public Long getTotalCount()throws Exception;
+	//select
+	public BoardVO getSelect(BoardVO boardVO)throws Exception;
+	//Insert
+	public int setInsert(BoardVO boardVO)throws Exception;
+	//Update
+	public int setUpdate(BoardVO boardVO)throws Exception;
+	//HitUpdate
+	public int setHitUpdate(BoardVO boardVO)throws Exception;
+	//Delete
+	public int setDelete(BoardVO boardVO)throws Exception;
+	
+	
+	
+	
+}
